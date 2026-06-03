cfc315e2b8521e7b54430bc865199a068167cba6
Erin Steiner <erin.steiner@noaa.gov>
2026-06-03 13:29:03 -0700

    Change process for updating data in app
    Instead of manually copying data into the data-raw folder, now there is
    a function that pulls over the latest data into the workspace and then
    processes the data into sysdata.R (which is the format used for the app
    itself)

--- app.R
+++ app.R
@@ -4,4 +4,7 @@
 
 pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 options("golem.app.prod" = TRUE)
+# run anytime data needs to be refreshed from G drive
+source("R/data_processing.R")
+prep_data_fun("2026-06-03")
 purcprod::run_app() # add parameters here (if any)
